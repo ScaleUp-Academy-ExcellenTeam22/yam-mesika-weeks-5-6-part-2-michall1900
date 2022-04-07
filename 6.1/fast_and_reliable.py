e35ee7ea6d36658a7ba552e96eb211d0e342507c
Michal Bucks <michall1900@gmail.com>
2022-04-07 17:53:12 +0300

    Change implementation to decorators change from time to timeit

--- 6.1/fast_and_reliable.py
+++ 6.1/fast_and_reliable.py
@@ -1,26 +1,31 @@
 from collections.abc import Callable
-from functools import partial
-from time import time
+from functools import partial, wraps
+import timeit
 from typing import Any, Iterable
 
 
-def average_runtime(*function_parameters: Any, number_of_runs: int, function: Callable[[Any], Any]) -> float:
+def average_run_time(runs_time: int) -> Callable[[Any], Any]:
     """
-    Return the average running time while running function "number of runs" times.
-    :param function_parameters: Parameters that function should receive.
-    :param number_of_runs: Time's number you want to run the function.
-    :param function: Function to check. It's return value will not return from this function.
+    Decorator that Return the average running time while running function "runs_time" times.
+    I used this site:
+    https://www.pythontutorial.net/advanced-python/python-decorator-arguments/
+    :param runs_time: Time's number you want to run the function.
     :return: Average time if number of runs > 0, otherwise, -1.
     """
-    sum_time = 0
-    for iterate in range(number_of_runs):
-        start = time()
-        function(*function_parameters)
-        end = time()
-        sum_time += (end-start)
-    return sum_time if number_of_runs > 0 else -1
+    def decorator(function: Callable[[Any], Any]) -> Callable[[Any], Any]:
+        @wraps(function)
+        def wrapper(*args: Any, **kwargs: Any) -> float:
+            sum_time = 0
+            for iterate in range(runs_time):
+                start = timeit.default_timer()
+                function(*args, **kwargs)
+                sum_time += timeit.default_timer()-start
+            return sum_time if runs_time > 0 else -1
+        return wrapper
+    return decorator
 
 
+@average_run_time(1000)
 def is_inside_iterable(item: Any, iterable: Iterable[Any]) -> bool:
     """
     Check if an item appeared inside an iterable.
@@ -42,8 +47,7 @@ def main_fast_and_reliable() -> None:
     with open("words.txt", "r") as file:
         file_in_list = file.read().split("\n")
     file_in_set = set(file_in_list)
-    compare_average_time = partial(average_runtime, "zwitterion", number_of_runs=1000,
-                                   function=is_inside_iterable)
+    compare_average_time = partial(is_inside_iterable, "zwitterion")
     print_result = lambda start_message_word, iterable: print(f"{start_message_word} average = ",
                                                               compare_average_time(iterable))
     print_result("list", file_in_list)
